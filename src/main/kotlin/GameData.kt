256dec282f2922f984a8c4fd4f9d44c4816efca1
akiraandy <andypschwartz@gmail.com>
2018-04-25 15:24:05 -0500

    create endpoint that returns a computer move and human move

---
+++ src/main/kotlin/GameData.kt
@@ -0,0 +1,5 @@
+import kotlinx.serialization.Optional
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GameData(val board: ArrayList<String>? = null, @Optional val input: Int? = null)
---
+++ src/test/kotlin/TTTEngineParserTest.kt
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class TTTEngineParserTest {
+
+    private val emptyBoardHumanMove: GameData = GameData(arrayListOf("0", "1", "2", "3", "4", "5", "6", "7", "8"), 4)
+    private val invalidInput: GameData = GameData(arrayListOf("X", "1", "2", "3", "4", "5", "6", "7", "8"), 0)
+    private val computerMakeMove: GameData = GameData(arrayListOf("X", "1", "2", "3", "4", "5", "6", "7", "8"), null)
+    private val computerFullBoard: GameData = GameData(arrayListOf("X", "X", "X", "X", "X", "X", "O", "O", "O"), null)
+    private val tttEngineParser = TTTEngineParser()
+
+    @Test
+    internal fun returnsANewBoard() {
+        val expected: List<String> = arrayListOf("0", "1", "2", "3", "X", "5", "6", "7", "8")
+        val newBoard = tttEngineParser.generateBoard(emptyBoardHumanMove.board, emptyBoardHumanMove.input)
+        assertEquals(expected, newBoard)
+    }
+
+    @Test
+    internal fun computerTurnReturnsNewBoard() {
+        val expected: List<String> = arrayListOf("X", "1", "2", "3", "O", "5", "6", "7", "8")
+        val newBoard = tttEngineParser.generateBoard(computerMakeMove.board, computerMakeMove.input)
+        assertEquals(expected, newBoard)
+    }
+
+    @Test
+    internal fun computerMoveReturnsSameBoardIfGameIsOver() {
+        val expected: List<String> = arrayListOf("X", "X", "X", "X", "X", "X", "O", "O", "O")
+        val newBoard = tttEngineParser.generateBoard(computerFullBoard.board, computerFullBoard.input)
+        assertEquals(expected, newBoard)
+    }
+
+    @Test
+    internal fun returnsSameBoardIfHumanMakesInvalidInput() {
+        val expected: List<String> = arrayListOf("X", "1", "2", "3", "4", "5", "6", "7", "8")
+        val newBoard = tttEngineParser.generateBoard(invalidInput.board, invalidInput.input)
+        assertEquals(expected, newBoard)
+    }
+}
