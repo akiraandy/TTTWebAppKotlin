d521df752a31062b0f85faed4d059cb15f9d0d81
Andy Schwartz <andypschwartz@gmail.com>
2018-05-01 14:35:56 -0500

    adjust gradle file and add argument parser to main (#5)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,12 @@
 import http.method.HttpMethod
 import http.router.Router
 import http.server.Server
+import http.utils.MainArgumentParser
 
 fun main(args : Array<String>) {
     val router = configureRouter()
-    val server = Server(5000, "./", router)
+    val argumentParser = parseArguments(args)
+    val server = Server(Integer.parseInt(argumentParser.get("PortNumber")), argumentParser.get("Directory"), router)
     server.run()
 }
 
@@ -12,4 +14,11 @@ fun configureRouter(): Router {
     val router = Router()
     router.addRoute("/move", listOf(HttpMethod.POST, HttpMethod.OPTIONS), GameController())
     return router
+}
+
+private fun parseArguments(args: Array<String>): Map<String, String> {
+    val argumentParser = MainArgumentParser()
+            .addFlag("-p", "PortNumber", "5000")
+            .addFlag("-d", "Directory", "./")
+    return argumentParser.parse(args)
 }
